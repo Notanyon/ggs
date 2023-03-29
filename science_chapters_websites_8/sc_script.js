4e77d08141bf9a41e3355752ee40a7d19805602b
Adityablogger <81465919+Adityablogger@users.noreply.github.com>
2023-03-29 19:22:18 +0530

    Introduced more accurate key validation system

--- science_chapters_websites_8/sc_script.js
+++ science_chapters_websites_8/sc_script.js
@@ -1,17 +1,43 @@
 const sc_8_1_key = "jdwak"; // Replace with your actual key
-const sc_8_2_key = ""; // Replace with your actual key
-const sc_8_3_key = ""; // Replace with your actual key
+const sc_8_2_key = "testing"; // Replace with your actual key
+const sc_8_3_key = "testing_s"; // Replace with your actual key
 const sc_8_4_key = ""; // Replace with your actual key
-      function sc_8_1() {
-        const keyInput = document.getElementById("keyInput").value;
+function sc_8_1() {
+  const keyInput = document.getElementById("keyInput").value;
 
-        if (keyInput === sc_8_1_key) {
-          alert("Key is valid!");
-          
-          // Do something else here if key is valid
-        } else {
-          alert("Invalid key!");
-          location.reload();
-          // Do something else here if key is invalid
-        }
-      }
+  if (keyInput === sc_8_1_key) {
+    alert("Key is valid!");
+    
+    // Do something else here if key is valid
+  } else {
+    alert("Invalid key!");
+    location.reload();
+    // Do something else here if key is invalid
+  }
+}
+function sc_8_2() {
+  const keyInput = document.getElementById("keyInput").value;
+
+  if (keyInput === sc_8_2_key) {
+    alert("Key is valid!");
+    
+    // Do something else here if key is valid
+  } else {
+    alert("Invalid key!");
+    location.reload();
+    // Do something else here if key is invalid
+  }
+}
+function sc_8_3() {
+  const keyInput = document.getElementById("keyInput").value;
+
+  if (keyInput === sc_8_3_key) {
+    alert("Key is valid!");
+    
+    // Do something else here if key is valid
+  } else {
+    alert("Invalid key!");
+    location.reload();
+    // Do something else here if key is invalid
+  }
+}
